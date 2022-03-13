533a73304efa9616b41f3529edd5718d2199ac04
Pedro <pedroaraujo_41@hotmail.com>
2022-03-13 18:42:23 +0000

    added default admin

--- src/main/java/ipvc/estg/westseatraceability/WestseaTraceabilityApplication.java
+++ src/main/java/ipvc/estg/westseatraceability/WestseaTraceabilityApplication.java
@@ -1,7 +1,14 @@
 package ipvc.estg.westseatraceability;
 
+import ipvc.estg.westseatraceability.dto.CreateUserDto;
+import ipvc.estg.westseatraceability.model.RoleEnum;
+import ipvc.estg.westseatraceability.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class WestseaTraceabilityApplication {
@@ -10,20 +17,10 @@ public class WestseaTraceabilityApplication {
         SpringApplication.run(WestseaTraceabilityApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner run(UserService userService) {
-//        return args -> {
-//
-//            User user1 = userService.saveUser(new User(null, "John Doe", "jdoe", "1234", new ArrayList<>()));
-//            User user2 = userService.saveUser(new User(null, "Jane Doe", "janedoe", "1234", new ArrayList<>()));
-//            User user3 = userService.saveUser(new User(null, "Jack Sparrow", "blackpearl", "1234", new ArrayList<>()));
-//            User user = userService.saveUser(new User(null, "Tony Stark", "irontony", "1234", new ArrayList<>()));
-//
-//            userService.addRoleToUser(user1.getId(), RoleEnum.ROLE_EMPLOYEE);
-//            userService.addRoleToUser(user1.getId(), RoleEnum.ROLE_ADMIN);
-//            userService.addRoleToUser(user2.getId(), RoleEnum.ROLE_EMPLOYEE);
-//            userService.addRoleToUser(user3.getId(), RoleEnum.ROLE_EMPLOYEE);
-//            userService.addRoleToUser(user.getId(), RoleEnum.ROLE_CLIENT);
-//        };
-//    }
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveUser(new CreateUserDto("Administrator", "admin", "1234", List.of(RoleEnum.ROLE_ADMIN)));
+        };
+    }
 }
--- src/main/java/ipvc/estg/westseatraceability/dto/CreateUserDto.java
+++ src/main/java/ipvc/estg/westseatraceability/dto/CreateUserDto.java
@@ -1,12 +1,14 @@
 package ipvc.estg.westseatraceability.dto;
 
 import ipvc.estg.westseatraceability.model.RoleEnum;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class CreateUserDto {
     private String name;
     private String username;
